c692583d929454efaa44f6c49f0e472e0f63ec03
Maggie Hibberd <maggiehibberd@gmail.com>
2021-04-30 19:37:43 +0100

    Test for number entry 5 completed and passing '5 -> Buzz'

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -4,5 +4,7 @@ def fizz_buzz (number)
   case
   when  (number % 3 == 0)
     return 'Fizz'
+  when 5 
+    'Buzz'
   end
 end
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -10,4 +10,7 @@ describe '#fizzbuzz' do
   it 'will take the number 9 and return the word "Fizz"' do 
     expect(fizz_buzz(9)).to eq 'Fizz'
   end
+  it 'will take the number 9 and return the word "Fizz"' do 
+    expect(fizz_buzz(5)).to eq 'Buzz'
+  end
 end
