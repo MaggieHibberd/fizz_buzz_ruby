87190fb447aa0531d196b439bee095a10696e5ef
Maggie Hibberd <maggiehibberd@gmail.com>
2021-04-30 19:50:42 +0100

    Code refactored and generalised where previously hardcoded

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -4,11 +4,7 @@ def fizz_buzz (number)
   case
   when  (number % 3 == 0)
     return 'Fizz'
-  when number == 5 
-    'Buzz'
-  when number == 10 
-    'Buzz'
-  when number == 20 
+  when (number % 5 == 0)
     'Buzz'
   end
 end
